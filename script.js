ca8d84157bbcca0babee81a3aa67f600186600f0
Munya3 <munya.maravanyika@yahoo.com>
2025-02-06 14:59:23 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function ShowThankyou(){
+    document.getElementById("showdiv").innerHTML = 
+    `
+        <div id="thankyou" class="box-thankyou">
+           <br><h2>❤️</h2>
+            <h2>Thank You!</h2> 
+            <h2>Feedback: Satisfied</h2>
+            <p>We'll use your Feedback to improve our <br> customer support</p>
+        </div>
+    `
+}
